19fe711c5280a106e607dde380447c32deaddea8
Vaibhav Rakshe <vaibhavrakshe9220@gmail.com>
2026-02-09 09:42:51 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
+// }
 
-export default nextConfig
+// export default nextConfig
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  }
+};
+
+export default nextConfig;
